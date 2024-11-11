6427a1cd7cbc30ce57abbd6380cc675218dd9b5f
TSKangetsu <x403070567@live.com>
2024-11-11 20:16:37 +0800

    add dev args

--- main.cpp
+++ main.cpp
@@ -11,12 +11,12 @@ using namespace V4L2Tools;
 
 int main(int argc, char const *argv[])
 {
-    if (argc >= 5) // width height fps qp
+    if (argc >= 6) //dev width height fps qp
     {
-        V4L2Encoder v4leOut("/dev/video1", {
-                                               std::atoi(argv[1]),
+        V4L2Encoder v4leOut(argv[1], {
                                                std::atoi(argv[2]),
                                                std::atoi(argv[3]),
+                                               std::atoi(argv[4]),
                                                1,
                                                false,
                                                V4L2_PIX_FMT_H264,
@@ -30,10 +30,10 @@ int main(int argc, char const *argv[])
         AVPacket pkt;
         TAVCodecCtx *AVCTX = new TAVCodecCtx;
         TAVCodecInfo AVINF = {
-            .qp = (char *)argv[4],
-            .width = std::atoi(argv[1]),
-            .height = std::atoi(argv[2]),
-            .framerate = std::atoi(argv[3]),
+            .qp = (char *)argv[5],
+            .width = std::atoi(argv[2]),
+            .height = std::atoi(argv[3]),
+            .framerate = std::atoi(argv[4]),
             .inputPixfmt = AV_PIX_FMT_NV12,
         };
         AVCodecInit(AVCTX, AVINF);
@@ -46,7 +46,7 @@ int main(int argc, char const *argv[])
                 std::copy(pkt.data, pkt.data + pkt.size, datain2.data.get());
                 datain2.size = pkt.size;
 
-                std::cout << thread.TimeDT << " " << datain2.size << " " << dataOut2.size << "\n\n";
+                // std::cout << thread.TimeDT << " " << datain2.size << " " << dataOut2.size << "\n\n";
             },
             (float)std::atoi(argv[3]));
 
